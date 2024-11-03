c0d6ee46039751b8d991d45433be9695e7bc88b6
mxmmsv <oikioikioikiabrams@gmail.com>
2024-11-03 22:26:19 +0300

    feat: add getSumToN task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -317,8 +317,13 @@ function getFibonacciNumber(index) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  let a = 0;
+  for (let i = 1; i <= n; i += 1) {
+    a += i;
+  }
+
+  return a;
 }
 
 /**
