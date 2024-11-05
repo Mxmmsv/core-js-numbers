35ebbd40e436fb6eb863ba576ad35748021b1af7
mxmmsv <oikioikioikiabrams@gmail.com>
2024-11-05 16:48:08 +0300

    feat: add isPowerOfTwo task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -357,8 +357,13 @@ function getSumOfDigits(num) {
  *   16  => true
  *   15  => false
  */
-function isPowerOfTwo(/* num */) {
-  throw new Error('Not implemented');
+function isPowerOfTwo(num) {
+  let pow = 2;
+  while (pow < num) {
+    const i = 2;
+    pow *= i;
+  }
+  return pow === num;
 }
 
 /**
